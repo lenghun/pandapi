5893e28b1631dd3ee569f41f3a67a32ae9d35b93
lenghun <lenghun@live.cn>
2026-01-09 12:57:54 +0800

    3

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -45,8 +45,8 @@ export class DatabaseClient {
     const placeholders = columns.map(() => '?').join(',');
     
     const sql = `INSERT INTO ${table} (${columns.join(',')}) VALUES (${placeholders})`;
-    console.error(sql);
-    console.error(values);
+    console.log(sql);
+    console.log(values);
     const result = await this.execute(sql, values);
     return result.meta.last_row_id;
   }
